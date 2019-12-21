a66516bc7d69bbf0099c44b42c06b13b67690e62
luddilo <ludvig@furhatrobotics.com>
2019-12-21 15:01:21 +0100

    0.2.1, changing create route to use create_v2 endpoint

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,2 @@
 export const CUSTOM_START_URL = "https://europe-west1-narratory-1.cloudfunctions.net/customStart"
-export const CREATE_AGENT_URL = "https://europe-west1-narratory-1.cloudfunctions.net/create"
+export const CREATE_AGENT_URL = "https://europe-west1-narratory-1.cloudfunctions.net/create_v2"
