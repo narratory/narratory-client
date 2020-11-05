bf1fe9c207b2e929ba7897a142e395fd67860212
Ludvig Linse <ludvig.linse@gmail.com>
2020-11-05 12:08:18 +0100

    Updated URLs to use new server

--- src/settings.ts
+++ src/settings.ts
@@ -1,13 +1,13 @@
 import { Language } from "./data/languages"
 
 export const API_VERSION = "2"
-export const CUSTOM_START_URL = "https://europe-west1-narratory-1.cloudfunctions.net/customStart"
-export const CREATE_AGENT_URL = "https://europe-west1-narratory-1.cloudfunctions.net/create_v" + API_VERSION 
-export const CREATE_AGENT_URL_LOCAL = "http://localhost:5000/narratory-1/europe-west1/create_v" + API_VERSION
-export const DEPLOY_AGENT_URL = "https://europe-west1-narratory-1.cloudfunctions.net/deploy_v" + API_VERSION 
-export const DEPLOY_AGENT_URL_LOCAL = "http://localhost:5000/narratory-1/europe-west1/deploy_v" + API_VERSION 
-export const VALIDATE_AGENT_URL = "https://europe-west1-narratory-1.cloudfunctions.net/validate_v" + API_VERSION
-export const VALIDATE_AGENT_URL_LOCAL = "http://localhost:5000/narratory-1/europe-west1/validate_v" + API_VERSION 
+export const CUSTOM_START_URL = "https://api.narratory.io/customStart"
+export const CREATE_AGENT_URL = "https://api.narratory.io/create_v" + API_VERSION 
+export const CREATE_AGENT_URL_LOCAL = "http://localhost:3000/create_v" + API_VERSION
+export const DEPLOY_AGENT_URL = "https://api.narratory.io/deploy_v" + API_VERSION 
+export const DEPLOY_AGENT_URL_LOCAL = "http://localhost:3000/deploy_v" + API_VERSION 
+export const VALIDATE_AGENT_URL = "https://api.narratory.io/validate_v" + API_VERSION
+export const VALIDATE_AGENT_URL_LOCAL = "http://localhost:3000/validate_v" + API_VERSION 
 export const DIALOGFLOW_RETRY_ATTEMPTS = 2
 export const DEFAULT_LANGUAGE = Language.EnglishUS
 export const DELAY_AFTER_TRAINING = 5000
